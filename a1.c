246a65d7c6f48c64aa9b3e9d435d7e76e04b2a67
Aashmun Gupta <aashmun.gupta@iitgn.ac.in>
2023-11-01 18:38:25 +0530

    Relocation

--- a1.c
+++ a1.c
@@ -1,4 +1,8 @@
 #include "test.h"
+#include <stdio.h>
+#include <string.h> // for testing generate_splits()
+
+
 
 #include <string.h> // for testing generate_splits()
 
@@ -10,8 +14,40 @@
  * Selections should be generated in lexicographic order.
  * a[0..k-1] is the smallest selection and a[n-k..n-1] is the largest.
  */
+
+
+void temp_selections(int a[], int n, int k, int b[], int id, int start, void *data, void (*process_selection)(int *b, int k, void *data))
+{
+    if (id == k)
+    {
+        process_selection(b, k, data);
+        return;
+    }
+
+    for (int i = start; i < n; i++)
+    {
+        b[id] = a[i];
+        temp_selections(a, n, k, b, id + 1, i + 1, data, process_selection);
+    }
+}
+
 void generate_selections(int a[], int n, int k, int b[], void *data, void (*process_selection)(int *b, int k, void *data))
 {
+<<<<<<< HEAD
+    temp_selections(a, n, k, b, 0, 0, data, process_selection);
+}
+
+void process_selection(int *b, int k, void *data)
+{
+    
+    for (int i = 0; i < k; i++)
+    {
+        printf("%d", b[i]);
+        if (i < k - 1)
+            printf(" ");
+    }
+    printf("\n");  
+=======
     b[0] = 2; b[1] = 1;
     process_selection(b, 2, data);
     b[0] = 2; b[1] = 6;
@@ -24,6 +60,7 @@ void generate_selections(int a[], int n, int k, int b[], void *data, void (*proc
     process_selection(b, 2, data);
     b[0] = 6; b[1] = 5;
     process_selection(b, 2, data);
+>>>>>>> ec9d14d7873c4ce5cf0a3b4249e7ebf57a7514fc
 }
 
 /*
@@ -34,6 +71,35 @@ void generate_selections(int a[], int n, int k, int b[], void *data, void (*proc
  * The dictionary parameter is an array of words, sorted in dictionary order.
  * nwords is the number of words in this dictionary.
  */
+<<<<<<< HEAD
+void process(char *split, void *info) {
+    printf("%s\n", split);
+}
+
+void generate_splits(const char *input, const char *dict[], int nwords, char *buf, int id, int src_id, void *data, void (*process)(char *, void *)) {
+    if (input[src_id] == '\0') {
+        buf[id] = '\0';
+        process(buf, data);
+        return;
+    }
+
+
+    for (int i = 0; i < nwords; i++) {
+        const char *word = dict[i];
+        int len = strlen(word);
+        if (strncmp(input, word, len) == 0) {
+            strcpy(buf, word);
+            generate_splits(input + len, dict, nwords, buf + len, id + len, src_id + len, data, process);
+            return;
+        }
+    }
+
+    buf[id] = input[src_id];
+    generate_splits(input, dict, nwords, buf + 1, id + 1, src_id + 1, data, process);
+}
+
+
+=======
 void generate_splits(const char *source, const char *dictionary[], int nwords, char buf[], void *data, void (*process_split)(char buf[], void *data))
 {
     strcpy(buf, "art is toil");
@@ -41,11 +107,69 @@ void generate_splits(const char *source, const char *dictionary[], int nwords, c
     strcpy(buf, "artist oil");
     process_split(buf, data);
 }
+>>>>>>> ec9d14d7873c4ce5cf0a3b4249e7ebf57a7514fc
 
 /*
  * Transform a[] so that it becomes the previous permutation of the elements in it.
  * If a[] is the first permutation, leave it unchanged.
  */
+<<<<<<< HEAD
+
+int find_greatestless(int arr[], int start, int end, int target) {
+    int index = -1;
+    for (int i = start; i <= end; i++) {
+        if (arr[i] < target && (index == -1 || arr[i] > arr[index])) {
+            index = i;
+        }
+    }
+    return index;
+}
+
+void reverse(int arr[], int start, int end) {
+    while (start < end) {
+
+        int temp  = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+    }
+}
+
+void previous_permutation(int a[], int n) {
+    int i = n - 2;
+
+    while (i >= 0 && a[i] <= a[i + 1]) {
+        i--;
+    }
+
+    if (i >= 0) {
+        int j ;
+        j = find_greatestless(a, i + 1, n - 1, a[i]);
+        int temp  = a[i];
+        a[i] = a[j];
+        a[j] = temp;  
+        reverse(a, i + 1, n - 1);
+    }
+}
+
+
+
+
+/* Write your tests here. Use the previous assignment for reference. */
+
+
+typedef struct {
+    int index;
+    int err;
+} state_t;
+
+
+static void test_selections_2165(int b[], int k, void *data)
+{
+    state_t *s = (state_t *)data;
+    s->err = 0;
+=======
 void previous_permutation(int a[], int n)
 {
     a[0] = 1;
@@ -71,6 +195,7 @@ static void test_selections_2165(int b[], int k, void *data)
         s->err = 0;
         s->first = 0;
     }
+>>>>>>> ec9d14d7873c4ce5cf0a3b4249e7ebf57a7514fc
     switch (s->index) {
     case 0:
         if ((b[0] != 2) || (b[1] != 1)) {
@@ -108,6 +233,19 @@ static void test_selections_2165(int b[], int k, void *data)
     ++(s->index);
 }
 
+<<<<<<< HEAD
+BEGIN_TEST(generate_selections) {
+    // TEST 1
+    int a[] = { 2, 1, 6, 5 };
+    int b[2];
+    state_t s2165 = { .index = 0, .err = 1 };
+    generate_selections(a, 4, 2, b, &s2165, test_selections_2165);
+    ASSERT(!s2165.err, "Failed on 2 1 6 5.");
+
+
+
+
+=======
 void count_selections(int b[], int k, void *data)
 {
     int *d = (int*)data;
@@ -153,11 +291,18 @@ BEGIN_TEST(generate_selections) {
 
     generate_selections(bb, 24, 12, b, &s, last_selection);
     ASSERT_ARRAY_VALUES_EQ(s.b, 12, "Failed on last of 24C12", 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23);
+>>>>>>> ec9d14d7873c4ce5cf0a3b4249e7ebf57a7514fc
 } END_TEST
 
 void test_splits_art(char buf[], void *data)
 {
     state_t *s = (state_t*)data;
+<<<<<<< HEAD
+    s->err = 0;
+    switch (s->index) {
+    case 0:
+        if (!strcmp(buf, "art is toil")) {
+=======
     if (s->first) {
         s->err = 0;
         s->first = 0;
@@ -165,17 +310,29 @@ void test_splits_art(char buf[], void *data)
     switch (s->index) {
     case 0:
         if (strcmp(buf, "art is toil")) {
+>>>>>>> ec9d14d7873c4ce5cf0a3b4249e7ebf57a7514fc
             s->err = 1;
         }
         break;
     case 1:
+<<<<<<< HEAD
+        if (!strcmp(buf, "artist oil")) {
+=======
         if (strcmp(buf, "artist oil")) {
+>>>>>>> ec9d14d7873c4ce5cf0a3b4249e7ebf57a7514fc
             s->err = 1;
         }
         break;
     default:
         s->err = 1;
     }
+<<<<<<< HEAD
+}
+
+BEGIN_TEST(generate_splits) {
+    //TEST 1
+    const char *a = "artistoil";
+=======
     ++(s->index);
 }
 
@@ -186,6 +343,7 @@ void count_splits(char buf[], void *data)
 }
 
 BEGIN_TEST(generate_splits) {
+>>>>>>> ec9d14d7873c4ce5cf0a3b4249e7ebf57a7514fc
     const char *dict[] = {
         "art",
         "artist",
@@ -193,6 +351,20 @@ BEGIN_TEST(generate_splits) {
         "oil",
         "toil"
     };
+<<<<<<< HEAD
+    int nwords = 5;
+    state_t s = { .index = 0, .err = 1 };
+    char buf[256];
+    generate_splits(a, dict, nwords, buf,0,0, &s, test_splits_art);
+    ASSERT(!s.err, "Failed on 'artistoil'.");
+} END_TEST
+
+BEGIN_TEST(previous_permutation) {
+    //TEST 1
+    int a[] = { 1, 6, 5, 2, 3, 4 };
+    previous_permutation(a, 6);
+    ASSERT_ARRAY_VALUES_EQ(a, 6, "Failed on 1 5 6 2 3 4.", 1, 6, 4, 5, 3, 2);
+=======
     const char *dict_a[] = {
         "a",
         "aa",
@@ -252,6 +424,7 @@ BEGIN_TEST(previous_permutation) {
 
     previous_permutation(aa+3, 3);
     ASSERT_ARRAY_VALUES_EQ(aa, 6, "Failed on last part of aa.", 1, 2, 3, 4, 6, 5);
+>>>>>>> ec9d14d7873c4ce5cf0a3b4249e7ebf57a7514fc
 } END_TEST
 
 int main()
@@ -264,3 +437,4 @@ int main()
         });
     return 0;
 }
+
--- connect4.c
+++ connect4.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdbool.h>
+
 /* Implement connect 4 on a 5 (columns) x 4 (rows) board. */
 enum {
     EMPTY = 0,
@@ -8,26 +11,371 @@ enum {
 typedef char board_t[4][5];
 typedef char player_t;
 
-int has_won(board_t board, player_t player)
-{}
+bool has_won(board_t board, player_t player) {
+// Check horizontal win
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (board[i][j] == player &&
+                board[i][j + 1] == player &&
+                board[i][j + 2] == player &&
+                board[i][j + 3] == player) {
+                return true;
+            }
+        }
+    }
 
-int is_full(board_t board)
-{}
+// Check vertical win
+    for (int j = 0; j < 5; j++) {
+        int i = 0;
+        if (board[i][j] == player &&
+            board[i + 1][j] == player &&
+            board[i + 2][j] == player &&
+            board[i + 3][j] == player) {
+            return true;
+        }
+    }
+
+// Check diagonal win (top-left to bottom-right)
+    for (int j = 0; j < 2; j++) {
+        int i = 0;
+        if (board[i][j] == player &&
+            board[i + 1][j + 1] == player &&
+            board[i + 2][j + 2] == player &&
+            board[i + 3][j + 3] == player) {
+            return true;
+        }
+    }
+
+// Check diagonal win (top-right to bottom-left)
+    for (int j = 4; j > 2; j--) {
+        int i = 0;
+        if (board[i][j] == player &&
+            board[i + 1][j - 1] == player &&
+            board[i + 2][j - 2] == player &&
+            board[i + 3][j - 3] == player) {
+            return true;
+        }
+    }
+
+    return false; // No win
+}
+
+bool is_full(board_t board) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (board[i][j] == EMPTY) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
 
 typedef struct {
     int col;
     int score;
 } move_t;
 
-move_t best_move(board_t board, player_t player)
-{}
+int board_score(board_t board, player_t player) {
+    int score = 0;
+    int maxConsecutive = 0;
+    int maxBlocking = 0; // Track potential opponent wins to block them
 
-void print_board(board_t board)
-{}
+    // Check the row
+    for (int i = 0; i < 4; i++) {
+        int consecutive = 0;
+        int blocking = 0;
+        for (int j = 0; j < 5; j++) {
+            if (board[i][j] == player) {
+                consecutive++;
+            } else if (board[i][j] == EMPTY) {
+                blocking++;
+            } else {
+                blocking = 0;
+            }
 
-int main()
-{
-    /* Your game play logic. */
-    /* The user should have the option to select red or blue player. */
+            if (consecutive == 3) {
+                score += 10; // Encourage forming a connect4
+            } else if (consecutive == 2 && blocking == 1) {
+                score += 3; // Encourage forming a connect4
+            } else if (blocking == 3) {
+                score += 5; // Encourage blocking opponent's connect4
+            } else if (blocking == 2) {
+                score += 2; // Encourage blocking opponent's potential connect4
+            }
+
+            if (consecutive > maxConsecutive) {
+                maxConsecutive = consecutive;
+            }
+            if (blocking > maxBlocking) {
+                maxBlocking = blocking;
+            }
+        }
+    }
+
+    // Check the column
+    for (int j = 0; j < 5; j++) {
+        int consecutive = 0;
+        int blocking = 0;
+        for (int i = 0; i < 4; i++) {
+            if (board[i][j] == player) {
+                consecutive++;
+            } else if (board[i][j] == EMPTY) {
+                blocking++;
+            } else {
+                blocking = 0;
+            }
+
+            if (consecutive == 3) {
+                score += 10; // Encourage forming a connect 4
+            } else if (consecutive == 2 && blocking == 1) {
+                score += 3; // Encourage forming a connect 4
+            } else if (blocking == 3) {
+                score += 5; // Encourage blocking opponent's connect 4
+            } else if (blocking == 2) {
+                score += 2; // Encourage blocking opponent's potential connect 4
+            }
+
+            if (consecutive > maxConsecutive) {
+                maxConsecutive = consecutive;
+            }
+            if (blocking > maxBlocking) {
+                maxBlocking = blocking;
+            }
+        }
+    }
+
+    // Check diagonal (top-left to bottom-right)
+    for (int j = 0; j < 2; j++) {
+        int i = 0;
+        int consecutive = 0;
+        int blocking = 0;
+        for (int k = 0; k < 4; k++) {
+            if (board[i + k][j + k] == player) {
+                consecutive++;
+            } else if (board[i + k][j + k] == EMPTY) {
+                blocking++;
+            } else {
+                blocking = 0;
+            }
+
+            if (consecutive == 3) {
+                score += 10; // Encourage forming a connect 4
+            } else if (consecutive == 2 && blocking == 1) {
+                score += 3; // Encourage forming a connect 4
+            } else if (blocking == 3) {
+                score += 5; // Encourage blocking opponent's connect 4
+            } else if (blocking == 2) {
+                score += 2; // Encourage blocking opponent's potential connect 4
+            }
+
+            if (consecutive > maxConsecutive) {
+                maxConsecutive = consecutive;
+            }
+            if (blocking > maxBlocking) {
+                maxBlocking = blocking;
+            }
+        }
+    }
+
+    // Check diagonal (top-right to bottom-left)
+    for (int j = 4; j > 2; j--) {
+        int i = 0;
+        int consecutive = 0;
+        int blocking = 0;
+        for (int k = 0; k < 4; k++) {
+            if (board[i + k][j - k] == player) {
+                consecutive++;
+            } else if (board[i + k][j - k] == EMPTY) {
+                blocking++;
+            } else {
+                blocking = 0;
+            }
+
+            if (consecutive == 3) {
+                score += 10; // Encourage forming a connect 4
+            } else if (consecutive == 2 && blocking == 1) {
+                score += 3; // Encourage forming a connect 4
+            } else if (blocking == 3) {
+                score += 5; // Encourage blocking opponent's connect 4
+            } else if (blocking == 2) {
+                score += 2; // Encourage blocking opponent's potential connect 4
+            }
+
+            if (consecutive > maxConsecutive) {
+                maxConsecutive = consecutive;
+            }
+            if (blocking > maxBlocking) {
+                maxBlocking = blocking;
+            }
+        }
+    }
+
+    if (maxConsecutive >= 3) {
+        score += 50; // Encourage forming a connect4
+    } else if (maxBlocking > 3) {
+        score += 100; // Encourage blocking opponent's connect 4
+    }
+
+    return score;
+}
+
+move_t best_move(board_t board, player_t player) {
+    move_t move;
+    move.col = -1;
+    move.score = player == RED ? -10000 : 10000; // Initialize with a very low/high score
+
+    // Check if the current player has won
+    if (has_won(board, player)) {
+        move.score = player == RED ? 1000 : -1000;
+        return move;
+    }
+
+    // Check if the board is full (it's a draw)
+    if (is_full(board)) {
+        move.score = player == RED ? 1000 : -1000;
+        return move;
+    }
+
+    if (has_won(board, RED) || has_won(board, BLUE) || is_full(board)) {
+        move.score = board_score(board, player);
+        return move;
+    }
+
+    for (int j = 0; j < 5; j++) {
+        for (int i = 0; i < 4; i++) {
+            if (board[i][j] == EMPTY) {
+                // Make the move
+                board[i][j] = player;
+
+                // Recursively call best_move for the opponent
+                move_t make_move = best_move(board, (player == RED) ? BLUE : RED);
+
+                // Undo the move
+                board[i][j] = EMPTY;
+
+                // Update the best move if this move has a better score
+                if ((player == RED && make_move.score > move.score) ||
+                    (player == BLUE && make_move.score < move.score)) {
+                    move.col = j;
+                    move.score = make_move.score;
+                }
+                break;
+            }
+        }
+    }
+
+    return move;
+}
+
+
+/* Function to print the current state of the game board */
+void print_board(board_t board) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 5; j++) {
+            switch (board[i][j]) {
+                case EMPTY:
+                    printf("  "); // Empty cell
+                    break;
+                case RED:
+                    printf("R "); // Red player's disc
+                    break;
+                case BLUE:
+                    printf("B "); // Blue player's disc
+                    break;
+            }
+        }
+        printf("\n");
+    }
+    printf("1 2 3 4 5\n"); // Column numbers
+}
+
+int main() {
+    board_t board;
+    player_t user;
+    player_t bot;
+
+    // Initialize the board to empty
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 5; j++) {
+            board[i][j] = EMPTY;
+        }
+    }
+
+    printf("Choose your color (1 for RED, 2 for BLUE): ");
+    int choice;
+    scanf("%d", &choice);
+
+    if (choice == 1) {
+        user = RED;
+        bot = BLUE;
+    } else {
+        user = BLUE;
+        bot = RED;
+    }
+    player_t current_player = RED;
+
+    while (1) {
+        if (current_player == user) {
+            int column;
+            int row;
+
+            // Ask the user to select a column until a valid one is chosen
+            while (1) {
+                printf("Your move (1-5): ");
+                scanf("%d", &column);
+                column--; // Adjust for 0-based indexing
+                row = 0;
+
+                // Check if the selected column is not full
+                while (row < 4) {
+                    if (board[row][column] == EMPTY) {
+                        break;
+                    }
+                    row++;
+                }
+
+                if (row < 4) {
+                    break; // Valid move, exit the loop
+                } else {
+                    printf("Column is full. Please select another column.\n");
+                }
+            }
+
+            board[row][column] = user;
+            print_board(board);
+
+            if (has_won(board, user)) {
+                printf("Congratulations! You win!\n");
+                break;
+            } else if (is_full(board)) {
+                printf("It's a draw!\n");
+                break;
+            }
+        } else {
+            printf("Computer's move\n");
+            move_t move = best_move(board, bot);
+            int row = 0;
+            while (row < 4) {
+                if (board[row][move.col] == EMPTY) {
+                    board[row][move.col] = bot;
+                    break;
+                }
+                row++;
+            }
+            print_board(board);
+
+            if (has_won(board, bot)) {
+                printf("Computer wins!\n");
+                break;
+            } else if (is_full(board)) {
+                printf("It's a draw!\n");
+                break;
+            }
+        }
+
+        current_player = (current_player == RED) ? BLUE : RED;
+    }
+
     return 0;
 }
